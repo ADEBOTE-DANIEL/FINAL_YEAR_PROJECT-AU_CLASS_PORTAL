460ab00de084a634099e3f0a1be812d7e805ecf6
unknown <dannyadebote16@gmail.com>
2024-12-18 23:20:16 +0100

    second commit

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -134,6 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles','static')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'student_portal/static')
 ]
